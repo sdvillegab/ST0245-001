69fdcb7b2122446689f1263a09eca45ecafaf4ff
Julian Andres Ramirez Jimenez <jaramirezj@eafit.edu.co>
2020-08-14 11:27:26 -0500

    Se implementa la funcionalidad para que se puedan leer varios archivos

--- proyecto/codigo/lectura.py
+++ proyecto/codigo/lectura.py
@@ -4,11 +4,14 @@ Created on Thu Aug 13 11:36:17 2020
 @author: Samuel y Julian
 """
 import csv
-path = open("data/0_test_balanced_5000.csv")
-lista = csv.reader(path)
 
+def leer_datos(fichero):
+    path = open("codigo/data/"+fichero)
+    lista = csv.reader(path)
 
-for i in lista:
-    print(i)    
+    for i in lista:
+        print(i)    
 
-path.close()
+    path.close()
+
+leer_datos("0_train_balanced_15000.csv")
