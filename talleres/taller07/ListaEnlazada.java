2c350066bdb34e221ed3de15dfccbcb546332ac3
sdvillegab <68928317+sdvillegab@users.noreply.github.com>
2020-09-18 10:48:46 -0500

    Se añade el método del palíndromo

--- talleres/taller07/ListaEnlazada.java
+++ talleres/taller07/ListaEnlazada.java
@@ -63,7 +63,7 @@ public class ListaEnlazada {
         }
 
 
-// Inserta un dato en la posición index
+	// Inserta un dato en la posición index
         public void insert(int data, int index) throws IndexOutOfBoundsException{
             if(index>=0 && index<=size){
                 Node buscar = first;
@@ -88,71 +88,100 @@ public class ListaEnlazada {
             }   
         }
 
-// Borra el dato en la posición index
-public void remove(int index)throws IndexOutOfBoundsException{
-        if (index >= size || index < 0)throw new IndexOutOfBoundsException();   
-	if(index == 0 ){
-            first = first.next;
-            size--;
-            return;
-        }
-        Node buscar = first;
-        int cont = 0;
-        while(cont<index-1){
-            buscar = buscar.next;
-            cont++;
-        }      
-        Node aux = buscar.next;
-        buscar.next = aux.next;
-        size--;    
-}
+	// Borra el dato en la posición index
+	public void remove(int index)throws IndexOutOfBoundsException{
+		if (index >= size || index < 0)throw new IndexOutOfBoundsException();   
+		if(index == 0 ){
+		    first = first.next;
+		    size--;
+		    return;
+		}
+		Node buscar = first;
+		int cont = 0;
+		while(cont<index-1){
+		    buscar = buscar.next;
+		    cont++;
+		}      
+		Node aux = buscar.next;
+		buscar.next = aux.next;
+		size--;    
+	}
 
-public void clear(){
-    this.first = null;
-    this.size = 0;
-}
+	public void clear(){
+	    this.first = null;
+	    this.size = 0;
+	}
 
-// Verifica si está un dato en la lista
-public boolean contains(int data){
-    int n = containsPos(data);
-    if(n==-1)return false;
-    else return true;
-}
+	// Verifica si está un dato en la lista
+	public boolean contains(int data){
+	    int n = containsPos(data);
+	    if(n==-1)return false;
+	    else return true;
+	}
 
-public int containsPos(int data){
-   Node buscar = first;
-   int cont = 0;
-   while(buscar != null){
-       if (buscar.data == data)return cont;
-       buscar = buscar.next;
-       cont++;
-   }
-   return -1;
-}
+	public int containsPos(int data){
+	   Node buscar = first;
+	   int cont = 0;
+	   while(buscar != null){
+	       if (buscar.data == data)return cont;
+	       buscar = buscar.next;
+	       cont++;
+	   }
+	   return -1;
+	}
 
-private static int maximo(Node nodo, int n){
-    if(nodo == null) return n;
-    System.out.println(nodo.data);
-    if(nodo.data>n){
-        n = nodo.data;
-    }
-    return maximo(nodo.next,n);
-}
+	private static int maximo(Node nodo, int n){
+	    if(nodo == null) return n;
+	    System.out.println(nodo.data);
+	    if(nodo.data>n){
+		n = nodo.data;
+	    }
+	    return maximo(nodo.next,n);
+	}
 
-public static int maximo (ListaEnlazada lista){
-    return maximo(lista.first,-999999999);
-}
+	public static int maximo (ListaEnlazada lista){
+	    return maximo(lista.first,-999999999);
+	}
 
-private static boolean comparar(Node nodo1,Node nodo2){
-    if(nodo1.next == null) return true;
-    if(nodo1.data != nodo2.data) return false;
-    else return comparar(nodo1.next,nodo2.next);
-}
+	private static boolean comparar(Node nodo1,Node nodo2){
+	    if(nodo1.next == null) return true;
+	    if(nodo1.data != nodo2.data) return false;
+	    else return comparar(nodo1.next,nodo2.next);
+	}
 
-public static boolean comparar(ListaEnlazada lista1, ListaEnlazada lista2){
-    if(lista1.size != lista2.size) return false;
-    else return comparar(lista1.first,lista2.first);
-}
+	public static boolean comparar(ListaEnlazada lista1, ListaEnlazada lista2){
+	    if(lista1.size != lista2.size) return false;
+	    else return comparar(lista1.first,lista2.first);
+	}
+	//Comprueba si la lista es un palindromo
+	public boolean palindromo(){
+		if(size<3)return false;
+		
+		Node aux = first;
+		Node other = null;
+
+		int n = 0;
+		int middle = size/2;	
+			
+		while(aux != null){
+			if(n == middle && size%2 != 0){
+				aux = aux.next;			
+				n++;
+				continue;				
+			}
+			if(n >= middle){
+				if(other.data != aux.data) return false;
+				other = other.next;
+			}else{
+				Node opt = other;
+				other = new Node(aux.data);
+				other.next = opt;
+			}	
+			aux = aux.next;			
+			n++;
+		}
+		return true;
+	}
 
 
 }
