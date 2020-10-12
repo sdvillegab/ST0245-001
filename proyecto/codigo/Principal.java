beea82d4fb594b8b169549e02b1a7b7cd8a69b0a
sdvillegab <68928317+sdvillegab@users.noreply.github.com>
2020-10-11 20:03:02 -0500

    Update and rename Principal to Principal.java

--- proyecto/codigo/Principal.java
+++ proyecto/codigo/Principal.java
@@ -3,7 +3,7 @@ public class Principal{
 
     public static void main(String args []){
         
-        Valor giniresultante = CargarDatos.inicio("0_train_balanced_15000.csv", 15001);
+        Valor giniresultante = CargarDatos.inicio("data\0_train_balanced_15000.csv", 15001);
         System.out.println(giniresultante.getVariablePadre()+" == "+giniresultante.getNombreValor()+" ig: "+giniresultante.getIndiceGini());
         
     }
