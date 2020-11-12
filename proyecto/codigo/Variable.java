1e8ab0573889a6c60ad2ec290dac4a88262bc10c
sdvillegab <68928317+sdvillegab@users.noreply.github.com>
2020-11-12 13:53:23 -0500

    Update Variable.java

--- proyecto/codigo/Variable.java
+++ proyecto/codigo/Variable.java
@@ -5,9 +5,11 @@ public class Variable{
     private String nombreVariable;
     private TreeSet<String> recolectorDeValores = new TreeSet<String>();
     private Valor [] valores;
+    int index;
     
-    public Variable(String nombreVariable){
+    public Variable(String nombreVariable, int index){
         this.nombreVariable = nombreVariable;
+        this.index =  index;
     }
 
     public void agregarValor(String nombreValor){
@@ -17,15 +19,21 @@ public class Variable{
     public Valor [] getValores(){
         return valores;
     }
+    public String getNombreVariable(){
+        return nombreVariable;
+    }
 
    
     public void llenarValores(){
         valores = new Valor[recolectorDeValores.size()];
         int i = 0;
         for(String val: recolectorDeValores){
-            valores [i] = new Valor(val, nombreVariable);
+            valores [i] = new Valor(val, nombreVariable,index);
             i++; 
         }
+    }
+    public void eliminarValores(){
+        recolectorDeValores.clear();
     }
 
 }
