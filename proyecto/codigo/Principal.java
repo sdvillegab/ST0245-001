00c87c7f2086cc4638fb0fadfdf33e540e257b83
sdvillegab <68928317+sdvillegab@users.noreply.github.com>
2020-11-12 13:55:03 -0500

    Update Principal.java

--- proyecto/codigo/Principal.java
+++ proyecto/codigo/Principal.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Principal{
 
     public static void main(String args []){        
-       Arbol arbol = CargarDatos.Inicio("0_train_balanced_15000.csv");
+       Arbol arbol = CargarDatos.Inicio("data/0_train_balanced_15000.csv");
        double porcentajes [] = CargarDatos.Test("1_test_balanced_15000.csv", arbol);
        System.out.println("El algoritmo tiene un porcentaje "+porcentajes[0]+" de acierto");
        System.out.println("El algoritmo tiene un porcentaje "+porcentajes[1]+" de fallo");
