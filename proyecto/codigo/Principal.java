eaac25e0cf09a60a5f7a91f59ab348e9a52daefd
sdvillegab <68928317+sdvillegab@users.noreply.github.com>
2020-11-12 13:52:18 -0500

    Update Principal.java

--- proyecto/codigo/Principal.java
+++ proyecto/codigo/Principal.java
@@ -1,10 +1,12 @@
+import java.util.*;
 
 public class Principal{
 
-    public static void main(String args []){
-        
-        Valor giniresultante = CargarDatos.inicio("data\0_train_balanced_15000.csv", 15001);
-        System.out.println(giniresultante.getVariablePadre()+" == "+giniresultante.getNombreValor()+" ig: "+giniresultante.getIndiceGini());
-        
+    public static void main(String args []){        
+       Arbol arbol = CargarDatos.Inicio("0_train_balanced_15000.csv");
+       double porcentajes [] = CargarDatos.Test("1_test_balanced_15000.csv", arbol);
+       System.out.println("El algoritmo tiene un porcentaje "+porcentajes[0]+" de acierto");
+       System.out.println("El algoritmo tiene un porcentaje "+porcentajes[1]+" de fallo");
+       System.out.println("El algoritmo tiene un porcentaje "+Math.abs(100-(porcentajes[1]+porcentajes[0]))+" de marguen de error");
     }
 }
