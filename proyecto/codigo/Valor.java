70882db58f24118f192240df6c4e6790e0b3be3d
sdvillegab <68928317+sdvillegab@users.noreply.github.com>
2020-11-12 13:52:52 -0500

    Update Valor.java

--- proyecto/codigo/Valor.java
+++ proyecto/codigo/Valor.java
@@ -7,9 +7,10 @@ public class Valor {
     private int nodoNo_cant1;
     private int nodoNo_cant0; 
     private float indiceGini;
+    private int index;
 
 
-    public Valor(String nombreValor, String variablePadre){
+    public Valor(String nombreValor, String variablePadre,int index){
         this.nombreValor = nombreValor;
         this.variablePadre = variablePadre;
         nodoSi_cant1 = 0;
@@ -17,6 +18,7 @@ public class Valor {
         nodoNo_cant1 = 0;
         nodoNo_cant0 = 0;
         indiceGini = 999999999;
+        this.index = index;
     }
 
     public String getNombreValor(){
@@ -26,49 +28,42 @@ public class Valor {
     public String getVariablePadre() {
         return variablePadre;
     }
+
+    public int getIndex(){
+        return this.index;
+    }
     
 
     public void masNodoSi_cant1(){
         nodoSi_cant1++;
+        calcularIndiceGini();
     }
     public void masNodoSi_cant0(){
         nodoSi_cant0++;
+        calcularIndiceGini();
     }
     public void masNodoNo_cant1(){
         nodoNo_cant1++;
+        calcularIndiceGini();
     }
     public void masNodoNo_cant0(){
         nodoNo_cant0++;
+        calcularIndiceGini();
     }
 
     public void calcularIndiceGini(){
-
         float totalSi = nodoSi_cant0+nodoSi_cant1;
         float totalNo = nodoNo_cant0+nodoNo_cant1;
-
         float p0Si, p1Si, p0No, p1No, igSi, igNo, igPon;
-
         p0Si = nodoSi_cant0/totalSi;
         p1Si = nodoSi_cant1/totalSi;
-
         p0No = nodoNo_cant0/totalNo;
         p1No = nodoNo_cant1/totalNo;
-
         igSi = 1 - ((p0Si*p0Si)+(p1Si*p1Si));
-
         igNo = 1 - ((p0No*p0No)+(p1No*p1No));
-
         igPon = ( (totalNo*igNo) +(totalSi*igSi) ) / (totalNo+totalSi) ;
-        this.indiceGini = igPon;
-        
-        /*System.out.println("nodoSi_cant0: "+nodoSi_cant0+" nodoSi_cant1 "+nodoSi_cant1
-        + " nodoNo_cant0: "+nodoNo_cant0+" nodoNo_cant1: "+nodoNo_cant1);
-        
-        this.indiceGini = igPon;
-        
-        System.out.println( "nombre: "+  this.nombreValor +  " p0Si: "+p0Si+" p1Si: " +p1Si+ " p0No: "+p0No+" p1No "+p1No);*/
-        
-        System.out.println(variablePadre+" == "+nombreValor+" ig: "+ igPon);
+        this.indiceGini = igPon;       
+        //System.out.println(variablePadre+" == "+nombreValor+" ig: "+ igPon);
     }
 
     public float getIndiceGini(){
